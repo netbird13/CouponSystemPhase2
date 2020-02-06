7ba02e472591d4bd7d55343ec22a2df0b37d990a
Avi Koplovich <netbird247@gmail.com>
2020-02-06 15:11:51 +0200

    Update Client.java

--- src/main/java/com/AK/CouponSystemPhase2/beans/Client.java
+++ src/main/java/com/AK/CouponSystemPhase2/beans/Client.java
@@ -11,18 +11,11 @@ import javax.persistence.Table;
 public class Client {
 
 	private long id;
-	private String email;
-	private String password;
+	
 	
 	//Empty CTOR
 	public Client() {
 	}
-
-	// partial CTOR
-	public Client(String email, String password) {
-		this.email = email;
-		this.password = password;
-	}	
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -32,26 +25,6 @@ public class Client {
 
 	public void setId(long id) {
 		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	
-	
-	
+	}	
 	
 }
